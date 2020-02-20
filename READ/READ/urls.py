36c37feacbc77353126c71c1eb98b33351b7e370
coreprinciple97 <ishrat.jahan16@northsouth.edu>
2020-02-20 20:48:37 +0600

    urls.py created in idex app

--- READ/READ/urls.py
+++ READ/READ/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', include('index.urls')),
 ]
