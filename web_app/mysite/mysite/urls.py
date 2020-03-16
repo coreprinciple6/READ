30d5d323c6859083c715721c3404cd65f4cf805b
coreprinciple97 <ishrat.jahan16@northsouth.edu>
2020-03-17 00:22:39 +0600

    url pattern for all allauth added

--- web_app/mysite/mysite/urls.py
+++ web_app/mysite/mysite/urls.py
@@ -21,4 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('read/', include('read.urls')),
     path('admin/', admin.site.urls),
+    # path('', include('social_django.urls', namespace='social')),
+    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
