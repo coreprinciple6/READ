7b643c81e615cb693c669c5dfa878adda87bbefc
Mehran Siddiqui <mehransiddiqui@MacBook-Pro.local>
2020-05-30 16:39:48 +0600

    fixed foreign key bug

--- web_app/mysite/read/views.py
+++ web_app/mysite/read/views.py
@@ -19,9 +19,9 @@ import json
 # from rest_framework import authentication, permissions
 # from rest_framework.permissions import IsAuthenticated
 from django.conf import settings
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-# from rest_framework.authtoken.models import Token
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 # from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from django.views.decorators.csrf import csrf_exempt
 
@@ -31,13 +31,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 #---------- for creating token when user is created
-# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-# def create_auth_token(sender, instance=None, created=False, **kwargs):
-    # if created:
-        # Token.objects.create(user=instance)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
-    # for user in User.objects.all():
-        # Token.objects.get_or_create(user=user)
+    for user in User.objects.all():
+        Token.objects.get_or_create(user=user)
 # ===============================================
 # Miscellaneous functions
 # ===============================================
